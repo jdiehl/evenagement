f65e3afca2c7845e2e13ca003c8eede160fdc6fe
Jonathan Diehl <jonathan.diehl@gmail.com>
2021-02-20 19:41:31 +0100

    added automatic dark mode

--- styles/theme.js
+++ styles/theme.js
@@ -1,8 +1,20 @@
 import { createMuiTheme } from '@material-ui/core/styles'
+import getHours from 'date-fns/getHours'
 // import red from '@material-ui/core/colors/red'
+
+function paletteType() {
+  const hours = getHours(new Date())
+  if (hours < 6 || hours > 18) {
+    return 'dark'
+  }
+  return 'light'
+}
 
 // Create a theme instance.
 const theme = createMuiTheme({
+  palette: {
+    type: paletteType(),
+  },
   // palette: {
   //   primary: {
   //     main: '#556cd6',
