a97ac409105b2cbcd6878e76eb172b0ea6872725
Jonathan Diehl <jonathan.diehl@gmail.com>
2021-02-21 10:55:52 +0100

    improved footer

--- components/MainFooter.tsx
+++ components/MainFooter.tsx
@@ -1,10 +1,13 @@
 import { makeStyles } from '@material-ui/core/styles'
 import Container from '@material-ui/core/Container'
+import Divider from '@material-ui/core/Divider'
 import Typography from '@material-ui/core/Typography'
+import Grid from '@material-ui/core/Grid'
+import Link from '@material-ui/core/Link'
 
 const useStyles = makeStyles(theme => ({
   footer: {
-    backgroundColor: theme.palette.grey[100],
+    backgroundColor: theme.palette.background.default,
     // marginTop: theme.spacing(8),
     padding: theme.spacing(6, 0),
   },
@@ -15,9 +18,16 @@ export default function MainFooter() {
   return (
     <footer className={classes.footer}>
       <Container maxWidth="lg">
-        <Typography variant="subtitle2" align="center" gutterBottom>
-          Evenagement
-        </Typography>
+        <Grid container spacing={2} justify="center">
+          <Grid item xs={12}>
+            <Divider variant="middle" />
+          </Grid>
+          <Grid item xs={12}>
+            <Typography align="center">
+              <Link href="https://github.com/jdiehl/evenagement" target="_blank">Evenagement on GitHub</Link>
+            </Typography>
+          </Grid>
+        </Grid>
       </Container>
     </footer>
   )
