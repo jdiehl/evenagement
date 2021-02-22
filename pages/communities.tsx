d76991ba9df609387ce27e03d8f64299aa7d0935
Jonathan Diehl <jonathan.diehl@gmail.com>
2021-02-22 16:44:24 +0100

    load communities from firestore

--- pages/communities.tsx
+++ pages/communities.tsx
@@ -1,10 +1,21 @@
+import { useEffect, useState } from 'react'
+import { collection } from '../services/firebase'
+import { Data } from '../services/collections'
 import Main from '../components/Main'
 import CommunityGrid from '../components/CommunityGrid'
+import Loading from '../components/Loading'
 
 export default function Communities() {
+  const [communities, setCommunities] = useState(null)
+
+  useEffect(() => {
+    const ref = collection<Data.Community>('communities')
+    const unsub = ref.onSnapshot(docs => setCommunities(docs))
+    return () => unsub()
+  })
   return (
     <Main>
-      <CommunityGrid />
+      {communities ? <CommunityGrid communities={communities}/> : <Loading/>}
     </Main>
   )
 }
