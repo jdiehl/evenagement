91a8c760bc5d144204a6dbdc237d5dc36c21a4c6
Jonathan Diehl <jonathan.diehl@gmail.com>
2021-03-07 11:40:50 +0100

    removed box

--- components/LoginButton.tsx
+++ components/LoginButton.tsx
@@ -1,5 +1,4 @@
 import Button from '@material-ui/core/Button'
-import Box from '@material-ui/core/Box'
 import { useState } from 'react'
 import Modal from '@material-ui/core/Modal'
 import LoginWindow from './LoginWindow/LoginWindow'
@@ -9,11 +8,11 @@ export default function LoginButton() {
   const [loginOpen, setLoginOpen] = useState(false)
 
   return (
-    <Box>
+    <>
       <Button color="inherit" onClick={() => setLoginOpen(true)}>Sign in</Button>
       <Modal open={loginOpen} onClose={() => setLoginOpen(false)}>
         <LoginWindow />
       </Modal>
-    </Box>
+    </>
   )
 }
