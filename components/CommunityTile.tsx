00bbdf195513e1c254721fb699b1b88ab8a1ddc4
Jonathan Diehl <jonathan.diehl@gmail.com>
2021-03-14 13:38:46 +0100

    fixed tile layout

--- components/CommunityTile.tsx
+++ components/CommunityTile.tsx
@@ -7,9 +7,9 @@ interface CommunityTileProps {
 // eslint-disable-next-line react/display-name, react/prop-types
 export default function CommmunityTile({ community }: CommunityTileProps) {
   return (
-    <div className="relative inline-block shadow-lg rounded-xl">
-      <img src={community.image} alt={community.name} className="rounded-xl" />
-      <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 rounded-b-xl px-4 py-2">
+    <div className="relative inline-block overflow-hidden shadow-lg rounded-xl h-64">
+      <img src={community.image} alt={community.name} className="w-full h-full object-cover" />
+      <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 px-4 py-2">
         <h2 className="text-2xl text-white font-medium truncate">{community.name || 'Unnamed Community'}</h2>
         <p className="text-gray-200 truncate">{community.description}</p>
       </div>
