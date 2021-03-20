79d95f005bceb4a277a0ef3ff52b1b98538f248d
Jonathan Diehl <jonathan.diehl@gmail.com>
2021-03-20 13:33:17 +0100

    added more tags to the button

--- components/atoms/Button.tsx
+++ components/atoms/Button.tsx
@@ -3,28 +3,31 @@ import { MouseEventHandler, PropsWithChildren } from 'react'
 type ButtonType = 'fill' | 'outline'
 
 interface ButtonProps {
+  tag?: 'a' | 'button' | 'input'
   label?: string
   type?: ButtonType
   href?: string
+  className?: string
   onClick?: MouseEventHandler
 }
 
 function extraStylesForType(type: ButtonType) {
   switch (type) {
-    case 'fill': return ['text-white', 'bg-primary', 'hover:bg-primary-dark', 'border', 'border-transparent']
-    case 'outline': return ['text-primary', 'hover:text-white', 'bg-transparent', 'hover:bg-primary-dark', 'border', 'border-solid', 'border-primary', 'hover:border-primary-dark']
+    case 'fill': return 'text-white bg-primary hover:bg-primary-dark border border-transparent'
+    case 'outline': return 'text-primary hover:text-white bg-transparent hover:bg-primary-dark border border-solid border-primary hover:border-primary-dark'
   }
 }
 
 function stylesForType(type: ButtonType) {
-  let styles = ['transition-all', 'px-6', 'py-2', 'select-none', 'text-base', 'font-medium', 'rounded', 'shadow', 'hover:shadow-lg', 'outline-none', 'focus:outline-none', 'cursor-pointer']
-  styles = styles.concat(extraStylesForType(type))
-  return styles.join(' ')
+  const styles = 'transition-all inline-block px-6 py-1 select-none text-center text-base font-medium rounded shadow hover:shadow-lg outline-none focus:outline-none cursor-pointer'
+  return styles + ' ' + extraStylesForType(type)
 }
 
-export default function Button({ children, label, type, ...props }: PropsWithChildren<ButtonProps>) {
-  const className = stylesForType(type || 'fill')
-  return (
-    <a className={className} {...props}>{label || children}</a>
-  )
+export default function Button({ children, label, type, tag, ...props }: PropsWithChildren<ButtonProps>) {
+  props.className = (props.className || '') + ' ' + stylesForType(type || 'fill')
+  switch (tag || 'a') {
+    case 'a': return <a {...props}>{label || children}</a>
+    case 'button': return <button {...props}>{label || children}</button>
+    case 'input': return <input type="submit" value={label} {...props} />
+  }
 }
