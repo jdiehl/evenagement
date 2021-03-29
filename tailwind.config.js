29ef8417c549b79249c9d322aa62370b45457dd9
Jonathan Diehl <jonathan.diehl@gmail.com>
2021-03-29 17:21:08 +0200

    added Toast component

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,16 @@ module.exports = {
           light: '#4B7FD2', // colors.blue[500],
           dark: '#2958A3', // colors.blue[700],
         },
+        error: {
+          DEFAULT: '#FF2D2D',
+          light: '#FF5454',
+          dark: '#E20505',
+        },
+        warning: {
+          DEFAULT: '#FF882D',
+          light: '#FF9E54',
+          dark: '#FF6F00',
+        }
       }
     },
   },
