ab082b7820383287dcd24533be6dbd8be95fdab0
Jan-Peter Krämer <jpkraemer@me.com>
2021-03-30 21:39:46 +0200

    changed event type to any

--- lib/useBinding.ts
+++ lib/useBinding.ts
@@ -2,11 +2,11 @@ import { ChangeEvent, Dispatch, SetStateAction, useState } from 'react'
 
 // type OnChangeHelper<T> = (prop: keyof T) => (ev: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => void
 
-type Binding<T> = (prop: keyof T) => { onChange: (ev: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => void, value: T[typeof prop] }
+type Binding<T> = (prop: keyof T) => { onChange: (ev: ChangeEvent<any>) => void, value: T[typeof prop] }
 
 export default function useBinding<T>(val: T): [T, Binding<T>, Dispatch<SetStateAction<T>>] {
   const [value, setValue] = useState(val)
-  const onChange = (prop: keyof T) => (ev: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => setValue({ ...value, [prop]: ev.target.value })
+  const onChange = (prop: keyof T) => (ev: ChangeEvent<any>) => setValue({ ...value, [prop]: ev.target.value })
   const binding = (prop: keyof T) => ({
     onChange: onChange(prop),
     value: value[prop]
