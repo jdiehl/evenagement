a867d799c8b1057a3b063f35e6d8590a10338c08
Jonathan Diehl <jonathan.diehl@gmail.com>
2021-04-20 17:57:55 +0200

    fixed storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,18 @@
-const path = require('path')
-
 module.exports = {
   stories: [
-    '../components/**/*.stories.@(js|jsx|ts|tsx)',
-    '../pages/**/*.stories.@(js|jsx|ts|tsx)'
+    '../src/**/*.stories.@(js|jsx|ts|tsx)',
   ],
   addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-actions',
     '@storybook/addon-links',
-    '@storybook/addon-essentials'
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
+    },
   ],
 }
