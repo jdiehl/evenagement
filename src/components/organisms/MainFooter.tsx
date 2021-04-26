f347fc98c9cc3b4b55552116caf5937885e25672
Jan-Peter Krämer <jpkraemer@me.com>
2021-04-26 23:00:05 +0200

    Community Page Design ready with mocks and room for refactoring

--- src/components/organisms/MainFooter.tsx
+++ src/components/organisms/MainFooter.tsx
@@ -1,7 +1,7 @@
 
 export default function MainFooter({ useTransparentBackground }: { useTransparentBackground?: boolean }) {
   return (
-    <div className={'py-12 mt-8 ' + (useTransparentBackground ? 'bg-background-shaded' : 'bg-background-dark')}>
+    <div className={'py-12 ' + (useTransparentBackground ? 'bg-background-shaded' : 'bg-background-dark')}>
       <footer className="container flex items-center mx-auto">
         <div className={'grid grid-cols-3 gap-4 text-sm w-1/2 mx-auto text-center ' + (useTransparentBackground ? 'text-white hover:text-gray-100' : 'text-primary hover:text-primary-dark')}>
           <a className="transition-all hover:underline" href="https://github.com/jdiehl/evenagement" target="_blank" rel="noreferrer">About</a>
--- src/pages/communities/[id]/index.tsx
+++ src/pages/communities/[id]/index.tsx
@@ -1,3 +1,4 @@
+import firebase from 'firebase/app'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { ArrowLeft, NotePencil } from 'phosphor-react'
@@ -5,6 +6,10 @@ import { ArrowLeft, NotePencil } from 'phosphor-react'
 import Button from '../../../components/atoms/Button'
 import Loading from '../../../components/atoms/Loading'
 import CommunityTile from '../../../components/molecules/CommunityTile'
+import HorizontalList from '../../../components/molecules/EventHorizontalList'
+import EventHorizontalList from '../../../components/molecules/EventHorizontalList'
+import EventTile from '../../../components/molecules/EventTile'
+import UserTile from '../../../components/molecules/UserTile'
 import Main from '../../../components/organisms/Main'
 import { Entities } from '../../../services/collections'
 import { useDoc } from '../../../services/firestore'
@@ -15,19 +20,65 @@ export default function Communities() {
   const id = router.query.id as string
 
   // subscribe to the community document
-  const community = useDoc(Entities.Community, id)
+  const communityDoc = useDoc(Entities.Community, id)
+  const community = communityDoc?.data()
+
+  const mockEvents = new Array(10).fill({ image: 'http://placekitten.com/400/400', title: 'Daily Session' })
+  const mockUsers = [
+    {
+      displayName: 'Jan-Peter Krämer',
+      photoURL: 'http://placekitten.com/400/400'
+    },
+    {
+      displayName: 'Jonathan Diehl'
+    },
+    {
+      displayName: 'Jonathan Diehl'
+    },
+    {
+      displayName: 'Jan-Peter Krämer',
+      photoURL: 'http://placekitten.com/400/400'
+    }
+  ] as firebase.User[]
 
   return (
     <Main>
-      <div className="flex flex-row">
+      {/* <div className="flex flex-row">
         <Link href="/communities">
           <Button><ArrowLeft /> Back to Communities</Button>
         </Link>
         <Link href={`/communities/${id}/edit`}>
           <Button tag="a"><NotePencil /> Edit</Button>
         </Link>
-      </div>
-      {community ? <CommunityTile community={community.data()}/> : <Loading/>}
+      </div> */}
+      {!communityDoc
+        ? <Loading/>
+        : <div className="flex flex-col flex-grow max-w-full">
+          <img src={community.image} alt={community.name} className="w-full h-60 object-cover" />
+          <div className="flex-grow flex">
+            <div className="flex-grow overflow-hidden p-4">
+              <p className="font-bold text-xl">{community.name}</p>
+              <p>{community.description}</p>
+              <p className="font-bold mt-8">Events</p>
+              <HorizontalList>
+                {mockEvents.map((event, i) => <EventTile event={event} key={i} />)}
+              </HorizontalList>
+              <p className="font-bold mt-8">Members</p>
+              <HorizontalList>
+                {mockUsers.map((user, i) => <UserTile user={user} key={i} />)}
+              </HorizontalList>
+            </div>
+            <div className="bg-primary-dark flex-none w-80 self-stretch text-white p-4">
+              <p className="font-bold text-xl">{community.name}</p>
+              <p><a className="underline" href="#">Home</a></p>
+              <p><a className="underline" href="#">Upcoming Events</a></p>
+              <p><a className="underline" href="#">Past Events</a></p>
+              <p><a className="underline" href="#">Members</a></p>
+              <Button className="w-full h-12 py-3 mt-4">Join Community</Button>
+            </div>
+          </div>
+        </div>
+      }
     </Main>
   )
 }
