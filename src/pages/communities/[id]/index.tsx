b1b09c9ecf5943c927ee3b520d41a2abdac2c7bf
Jonathan Diehl <jonathan.diehl@gmail.com>
2021-04-27 10:44:00 +0200

    Created CommunityDetails component

--- src/pages/communities/[id]/index.tsx
+++ src/pages/communities/[id]/index.tsx
@@ -1,13 +1,6 @@
-import firebase from 'firebase/app'
-import Link from 'next/link'
 import { useRouter } from 'next/router'
-import { ArrowLeft, NotePencil } from 'phosphor-react'
 
-import Button from '../../../components/atoms/Button'
-import HorizontalList from '../../../components/atoms/HorizontalList'
-import Loading from '../../../components/atoms/Loading'
-import EventTile from '../../../components/molecules/EventTile'
-import UserTile from '../../../components/molecules/UserTile'
+import CommunityDetails from '../../../components/organisms/CommunityDetails'
 import Main from '../../../components/organisms/Main'
 import { Entities } from '../../../services/collections'
 import { useDoc } from '../../../services/firestore'
@@ -21,62 +14,9 @@ export default function Communities() {
   const communityDoc = useDoc(Entities.Community, id)
   const community = communityDoc?.data()
 
-  const mockEvents = new Array(10).fill({ image: 'http://placekitten.com/400/400', title: 'Daily Session', date: new Date() })
-  const mockUsers = [
-    {
-      displayName: 'Jan-Peter Krämer',
-      photoURL: 'http://placekitten.com/400/400'
-    },
-    {
-      displayName: 'Jonathan Diehl'
-    },
-    {
-      displayName: 'Jonathan Diehl'
-    },
-    {
-      displayName: 'Jan-Peter Krämer',
-      photoURL: 'http://placekitten.com/400/400'
-    }
-  ] as firebase.User[]
-
   return (
     <Main>
-      {/* <div className="flex flex-row">
-        <Link href="/communities">
-          <Button><ArrowLeft /> Back to Communities</Button>
-        </Link>
-        <Link href={`/communities/${id}/edit`}>
-          <Button tag="a"><NotePencil /> Edit</Button>
-        </Link>
-      </div> */}
-      {!communityDoc
-        ? <Loading/>
-        : <div className="flex flex-col flex-grow max-w-full">
-          <img src={community.image} alt={community.name} className="w-full h-60 object-cover" />
-          <div className="flex-grow flex">
-            <div className="flex-grow overflow-hidden p-4">
-              <p className="font-bold text-xl">{community.name}</p>
-              <p>{community.description}</p>
-              <p className="font-bold mt-8">Events</p>
-              <HorizontalList>
-                {mockEvents.map((event, i) => <EventTile event={event} key={i} />)}
-              </HorizontalList>
-              <p className="font-bold mt-8">Members</p>
-              <HorizontalList>
-                {mockUsers.map((user, i) => <UserTile user={user} key={i} />)}
-              </HorizontalList>
-            </div>
-            <div className="bg-primary-dark flex-none w-80 self-stretch text-white p-4">
-              <p className="font-bold text-xl">{community.name}</p>
-              <p><a className="underline" href="#">Home</a></p>
-              <p><a className="underline" href="#">Upcoming Events</a></p>
-              <p><a className="underline" href="#">Past Events</a></p>
-              <p><a className="underline" href="#">Members</a></p>
-              <Button className="w-full h-12 py-3 mt-4">Join Community</Button>
-            </div>
-          </div>
-        </div>
-      }
+      <CommunityDetails community={community} />
     </Main>
   )
 }
