9b90b236471a1a9e094cd9f9160a65535f0791a1
Jonathan Diehl <jonathan.diehl@gmail.com>
2021-05-04 22:42:02 +0200

    merged storage with mock

--- src/services/storage.ts
+++ src/services/storage.ts
@@ -1,6 +1,14 @@
 import firebase from 'firebase/app'
 
-import storageMock from './storageMock'
+const storageMock = ({
+  child: (url: string) => ({
+    put: async (arg: any) => ({
+      ref: {
+        getDownloadURL: async () => { return 'https://placekitten.com/1200/300' }
+      }
+    })
+  } as unknown as firebase.storage.Reference)
+} as unknown as firebase.storage.Reference)
 
 export function storage() {
   if (process.env.NEXT_PUBLIC_USE_FIREBASE_EMULATOR) {
