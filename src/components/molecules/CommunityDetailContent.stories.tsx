9e684a8a0844fc5b0d0cf70085bb1fc4ae866411
Jonathan Diehl <jonathan.diehl@gmail.com>
2021-05-04 21:46:56 +0200

    added missing story

---
+++ src/components/molecules/CommunityDetailContent.stories.tsx
@@ -0,0 +1,23 @@
+import { Story, Meta } from '@storybook/react'
+
+import { MockDocument } from '../../lib/storybook'
+
+import CommunityDetailContent from './CommunityDetailContent'
+
+export default {
+  title: 'Molecules/CommunityDetailContent',
+  component: CommunityDetailContent,
+  argTypes: {}
+} as Meta
+
+// eslint-disable-next-line react/prop-types
+const Template: Story<any> = (args) => {
+  return <CommunityDetailContent community={MockDocument(args)}/>
+}
+
+export const Primary = Template.bind({})
+Primary.args = {
+  name: 'Some Community',
+  description: 'Do laborum labore Lorem sint.',
+  image: 'http://placekitten.com/800/200',
+}
