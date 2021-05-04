d0cfde6750a3aa0e83f53a443fafc791b88f6942
Jonathan Diehl <jonathan.diehl@gmail.com>
2021-05-04 20:44:51 +0200

    added mock data

--- src/components/molecules/CommunityDetailContent.tsx
+++ src/components/molecules/CommunityDetailContent.tsx
@@ -1,39 +1,19 @@
-import firebase from 'firebase/app'
 import Link from 'next/link'
 
-import { Community, Event } from '../../lib/store/types'
+import { Document } from '../../lib/store'
+import { Community } from '../../lib/store/types'
+import { mockEvents, mockUsers } from '../../services/mock'
 import HorizontalList from '../atoms/HorizontalList'
 
 import EventTile from './EventTile'
 import UserTile from './UserTile'
 
 interface CommunityDetailContentProps {
-  community: firebase.firestore.DocumentSnapshot<Community>
+  community: Document<Community>
 }
 
 export default function CommunityDetailContent({ community: communitySnapshot }: CommunityDetailContentProps) {
   const community = communitySnapshot.data()
-
-  const events = new Array(10).fill({
-    data: () => ({ image: 'http://placekitten.com/400/400', title: 'Daily Session', date: new Date() }),
-    id: '123'
-  } as firebase.firestore.DocumentSnapshot<Event>)
-  const users = [
-    {
-      displayName: 'Jan-Peter Krämer',
-      photoURL: 'http://placekitten.com/400/400'
-    },
-    {
-      displayName: 'Jonathan Diehl'
-    },
-    {
-      displayName: 'Jonathan Diehl'
-    },
-    {
-      displayName: 'Jan-Peter Krämer',
-      photoURL: 'http://placekitten.com/400/400'
-    }
-  ] as firebase.User[]
 
   return (
     <>
@@ -41,7 +21,7 @@ export default function CommunityDetailContent({ community: communitySnapshot }:
       <p>{community.description}</p>
       <p className="font-bold mt-8">Events</p>
       <HorizontalList>
-        {events.map((event, i) => <Link href={`/communities/${communitySnapshot.id}/${event.id}`} key={i}>
+        {mockEvents.map((event, i) => <Link href={`/communities/${communitySnapshot.id}/${event.id}`} key={i}>
           <a>
             <EventTile event={event.data()} />
           </a>
@@ -49,7 +29,7 @@ export default function CommunityDetailContent({ community: communitySnapshot }:
       </HorizontalList>
       <p className="font-bold mt-8">Members</p>
       <HorizontalList>
-        {users.map((user, i) => <UserTile user={user} key={i} />)}
+        {mockUsers.map((user, i) => <UserTile user={user} key={i} />)}
       </HorizontalList>
     </>
   )
---
+++ src/services/mock.ts
@@ -0,0 +1,23 @@
+import { CommunityEvent, Document, User } from '../lib/store'
+
+export const mockEvents = new Array(10).fill({
+  data: () => ({ image: 'http://placekitten.com/400/400', title: 'Daily Session', date: new Date() }),
+  id: '123'
+} as Document<CommunityEvent>)
+
+export const mockUsers = [
+  {
+    displayName: 'Jan-Peter Krämer',
+    photoURL: 'http://placekitten.com/400/400'
+  },
+  {
+    displayName: 'Jonathan Diehl'
+  },
+  {
+    displayName: 'Jonathan Diehl'
+  },
+  {
+    displayName: 'Jan-Peter Krämer',
+    photoURL: 'http://placekitten.com/400/400'
+  }
+] as User[]
