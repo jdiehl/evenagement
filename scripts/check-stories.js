1e0b42e69075c7b809d1cf0a3499c011d80b939a
Jonathan Diehl <jonathan.diehl@gmail.com>
2021-06-26 23:35:31 +0200

    let check-stories also check ui styling

--- scripts/check-stories.js
+++ scripts/check-stories.js
@@ -1,20 +1,54 @@
 #! /usr/bin/env node
 /* eslint-disable no-console */
 
-const { readdirSync } = require('fs')
+const { readdirSync, readFileSync, statSync } = require('fs')
 
-const components = readdirSync('./src/components').map(file => file.substr(0, file.indexOf('.')))
-const stories = readdirSync('./src/components/stories').map(file => file.substr(0, file.indexOf('.')))
-
-const WARN = '\x1b[33m'
-const NC = '\x1b[0m'
+const NON_UI_COMPONENTS = ['./src/content/', './src/fragments/', './src/layouts/', './src/pages/']
 
 let failed = 0
-for (const component of components) {
-  if (!component || component[0] === '.') continue
-  if (stories.includes(component)) continue
-  console.error(`Missing Storybook for Component: ${WARN}${component}${NC}`)
-  failed = 1
+
+function fail(message, file) {
+  console.error(`${message}: \x1b[33m${file}\x1b[0m`)
+  failed += 1
 }
 
-process.exit(failed)
+function readdirRec(dir) {
+  let res = []
+  const files = readdirSync(dir)
+  for (const file of files) {
+    if (statSync(dir + file).isDirectory()) {
+      res = res.concat(readdirRec(dir + file + '/'))
+    } else {
+      res.push(dir + file)
+    }
+  }
+  return res
+}
+
+function checkStories() {
+  const dir = './src/components/'
+  const components = readdirSync(dir)
+  const stories = readdirSync(`${dir}stories`).map(file => file.substr(0, file.indexOf('.')))
+
+  for (const component of components) {
+    if (stories.includes(component.substr(0, component.indexOf('.')))) continue
+    if (statSync(dir + component).isDirectory()) continue
+    fail('Missing Storybook for Component', dir + component)
+  }
+}
+
+function checkTailwind() {
+  for (const dir of NON_UI_COMPONENTS) {
+    const files = readdirRec(dir)
+    for (const file of files) {
+      const content = readFileSync(file).toString()
+      if (content.includes('className')) {
+        fail('Non-UI Component uses custom class', file)
+      }
+    }
+  }
+}
+
+checkStories()
+checkTailwind()
+process.exit(failed ? 1 : 0)
