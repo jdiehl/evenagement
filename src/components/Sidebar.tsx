69a7fbaf1744fb85fcc6af1bbdd6b21d4be6a666
Jonathan Diehl <jonathan.diehl@gmail.com>
2021-07-07 23:56:52 +0200

    fix sidebar width

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,7 +4,7 @@ import Container from './Container'
 
 export default function Sidebar({ children }: PropsWithChildren<{}>) {
   return (
-    <Container className="bg-primary-dark text-white w-80">
+    <Container className="w-80 flex-shrink-0 bg-primary-dark text-white">
       {children}
     </Container>
   )
