bbf33e8c3abf7821734e53b76d4c730941ca7902
Jonathan Diehl <jonathan.diehl@gmail.com>
2021-12-08 21:36:22 +0100

    Switched to Next Image Tag

--- src/modules/ui/CoverImage.tsx
+++ src/modules/ui/CoverImage.tsx
@@ -1,4 +1,4 @@
-// import Image from 'next/image'
+import Image from 'next/image'
 
 interface CoverImageProps {
   src: string
@@ -7,8 +7,6 @@ interface CoverImageProps {
 
 export default function CoverImage({ src, alt }: CoverImageProps) {
   return (
-    // next/image does not work on M1:
-    // <Image src={src} alt={alt} layout="fill" objectFit="cover" />
-    <img src={src} alt={alt} className="w-full h-full object-cover select-none" />
+    <Image src={src} alt={alt} layout="fill" objectFit="cover" />
   )
 }
